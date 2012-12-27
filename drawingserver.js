d320de6934c426400e3c8e4a28094061aa057e13
Rajiev Timal <rtimal@gmail.com>
2012-12-26 22:06:51 -0500

    First Commit

---
+++ drawingserver.js
@@ -0,0 +1,57 @@
+var express = require('express'),
+	app = express(),
+	server = require('http').createServer(app),
+	io = require('socket.io').listen(server),
+	hbs = require('express-hbs'),
+	users = {};
+
+//io.set('log level', 1);
+app.engine('hbs', hbs.express3({partialsDir: __dirname + '/views/partials'}));
+
+app.set('view engine', 'hbs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+
+server.listen(82);
+
+io.sockets.on('connection', function (socket) {
+
+	socket.on('getusers', function (data) {
+		socket.emit('userlist', JSON.stringify(users));
+	});
+
+	socket.on('join', function (user) {
+		u = JSON.parse(user);
+		socket.join(u.room);
+		users[u.uid] = u;
+		io.sockets.in(u.room).emit('adduser', user);
+	});
+
+	socket.on('leave', function (uid) {
+		io.sockets.in(users[uid].room).emit('removeuser', uid);
+		socket.leave(users[uid].room);
+		users[uid] = null;
+	 });
+
+	socket.on('mousedown', function (drawevent) {
+		this.broadcast.to(drawevent.room).emit('mousedown' , drawevent);
+	});
+
+	socket.on('mouseup', function (drawevent) {
+		this.broadcast.to(drawevent.room).emit('mouseup' , drawevent);
+	});
+
+	socket.on('mousemove', function (drawevent) {
+		this.broadcast.to(drawevent.room).emit('mousemove' , drawevent);
+	});
+
+	socket.on('changebrushcolor', function (colorinfo) {
+		users[colorinfo.uid].brushData.brushColor = colorinfo.c;
+		this.broadcast.to(users[colorinfo.uid].room).emit('changebrushcolor', colorinfo);
+	})
+
+	socket.on('changebrushwidth', function (widthinfo) {
+		users[widthinfo.uid].brushData.brushWidth = widthinfo.w;
+		this.broadcast.to(users[widthinfo.uid].room).emit('changebrushwidth', widthinfo);
+	})
+});
